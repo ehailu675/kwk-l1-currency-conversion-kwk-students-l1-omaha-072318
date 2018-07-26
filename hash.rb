d0ed730f3e26c936df473491d4410bceef23a171
ehailu675 <elisabethahailu@gmail.com>
2018-07-26 16:24:37 +0000

    automatically backed up by learn

--- hash.rb
+++ hash.rb
@@ -1,3 +1,7 @@
 # about_me_hash = { "name" => "Elisabeth", "age" => 17, "favorite animal" => "panda"}
 # puts about_me_hash["name"]
 # puts about_me_hash
+
+items { }
+items["24K Magic"] = 15.98
+puts items
