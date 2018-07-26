661657e18a979aa36eca58cfc3b288023afdc8af
ehailu675 <elisabethahailu@gmail.com>
2018-07-26 16:49:46 +0000

    automatically backed up by learn

--- hash.rb
+++ hash.rb
@@ -15,7 +15,7 @@
 # items["ACT prep book"] = 15
 # puts items
 
-# shopping_hash = {"24K_Magic" => 15.98, "crocs" => 35, "iphone" => 800.50, "ACT_prep_book" => 15}
+shopping_hash = {"24K_Magic" => 15.98, "crocs" => 35, "iphone" => 800.50, "ACT_prep_book" => 15}
 # puts shopping_hash.values
 
 
