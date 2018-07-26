d7994206a5ddec1e5ba684c155255805120467b3
ehailu675 <elisabethahailu@gmail.com>
2018-07-26 16:55:17 +0000

    automatically backed up by learn

--- hash.rb
+++ hash.rb
@@ -25,4 +25,5 @@ shopping_hash = {"24K_Magic" => 15.98, "crocs" => 35, "iphone" => 800.50, "ACT_p
 # end
 shopping_hash.each do |item_name, price|
   # puts "#{item_name}: #{price}"
+  puts item_name, price
 end
