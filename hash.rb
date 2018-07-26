1f0c5aa70eece7c274e7076caae641c6be110bb3
ehailu675 <elisabethahailu@gmail.com>
2018-07-26 16:18:37 +0000

    automatically backed up by learn

--- hash.rb
+++ hash.rb
@@ -1,3 +1,3 @@
-about_me_hash = { "name" => "Elisabeth", "age" => 17, "favorite animal" => "panda",}
+about_me_hash = { "name" => "Elisabeth", "age" => 17, "favorite animal" => "panda"}
 puts about_me_hash["name"]
 puts about_me_hash
